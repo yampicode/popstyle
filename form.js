58200f048ec54db1e44e06a9607da74652c753b5
Yampiero Mujica <jeanpiero01@hotmail.com>
2025-09-18 14:44:37 -0400

    Actualizar form.js

--- form.js
+++ form.js
@@ -7,9 +7,10 @@
             const email = form.email.value || 'No proporcionado';
             const service = form.service.value;
             const date = form.date.value;
+            const time = form.time.value;
             const message = form.message.value || 'No hay mensaje adicional';
             
-            const text = `Hola, me gustaría reservar una cita:%0A%0A*Nombre:* ${name}%0A*Teléfono:* ${phone}%0A*Email:* ${email}%0A*Servicio:* ${service}%0A*Fecha preferida:* ${date}%0A*Mensaje:* ${message}`;
+            const text = `Hola, me gustaría reservar una cita:%0A%0A*Nombre:* ${name}%0A*Teléfono:* ${phone}%0A*Email:* ${email}%0A*Servicio:* ${service}%0A*Fecha preferida:* ${date}%0A*Hora preferida:* ${time}%0A*Mensaje:* ${message}`;
             
             window.open(`https://wa.me/584120348988?text=${text}`, '_blank');
             form.reset();
